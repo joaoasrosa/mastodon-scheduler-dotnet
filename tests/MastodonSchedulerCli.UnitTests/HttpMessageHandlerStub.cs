33f6432a7b67ba25d4a5b7a82fa63eb982b0e7be
João Rosa <joaoasrosa@gmail.com>
2022-12-14 21:17:38 +0100

    Add stub class for HttpMessageHandler

---
+++ tests/MastodonSchedulerCli.UnitTests/HttpMessageHandlerStub.cs
@@ -0,0 +1,21 @@
+namespace MastodonSchedulerCli.UnitTests;
+
+public class HttpMessageHandlerStub : HttpMessageHandler
+{
+    private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _sendAsync;
+
+    public HttpMessageHandlerStub(
+        Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> sendAsync
+    )
+    {
+        _sendAsync = sendAsync;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        return await _sendAsync(request, cancellationToken);
+    }
+}
